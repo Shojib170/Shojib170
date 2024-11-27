06fa2c1f8cb95915c7fdca9eeedfeb35e92c724c
Cyber <xushojib@gmail.com>
2024-11-27 06:22:54 -0800

    Add files via upload

--- Cyber.py
+++ Cyber.py
@@ -46,7 +46,7 @@ def ua():
     aZ=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
     zA=random.choice(['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
     rx=random.randrange(1, 999)
-    xx=f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36]"
+    xx=f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
     return xx    
 
 
@@ -145,7 +145,7 @@ class create:
             'sec-fetch-site': 'none',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
-            'user-agent': ua(),
+            'user-agent': agent,
             'viewport-width': '980',}
         headers1 = {
             'authority': 'p.facebook.com',
@@ -161,7 +161,7 @@ class create:
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-origin',
             'upgrade-insecure-requests': '1',
-            'user-agent': ua(),}
+            'user-agent': agent,}
         OO = '\033[0;97m'
         for x in range(lim):
             self.loop += 1
